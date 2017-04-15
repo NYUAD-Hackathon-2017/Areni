70ddd0575bc77815b0da10105e22db7a6c114f5b
saanaa <khiarisanaaa@gmail.com>
2017-04-15 14:03:52 +0100

    chat2

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,3 @@
-'use strict';
-
-angular.module('Areni.teacher', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/teacher/:id', {
-    templateUrl: 'teacher/teacher.html',
-    controller: 'teacherCtrl'
-  });
-}])
-
-.controller('teacherCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-  // TODO: Get name from ID
-  console.log($routeParams.id)
-
-  $scope.teacher = {
-    "name": "Sara L.",
-    "subj": ["Algebra", "Trigonometry", "Geometry"],
-    "Lat":  52.420008,
-    "Lng":  13.404954,
-    "dist": 0.3,
-    "date": "Monday",
-    "loc": "Starbucks",
-    "img": "https://randomuser.me/api/portraits/women/13.jpg"
-  };
-
-  // Chat
 /**
  * Copyright 2015 Google Inc. All Rights Reserved.
  *
@@ -318,8 +291,3 @@ FriendlyChat.prototype.checkSetup = function() {
 window.onload = function() {
   window.friendlyChat = new FriendlyChat();
 };
-
-  
-
-
-}]);
